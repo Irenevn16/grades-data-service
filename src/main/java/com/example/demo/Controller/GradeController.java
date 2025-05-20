38fb7a4b77b4a0a3b35d4149a3e365bdd4eedf6c
Irenevn16 <irenevn16@gmail.com>
2025-05-20 17:24:12 +0200

    controllers añadidos

--- src/main/java/com/example/demo/Controller/GradeController.java
+++ src/main/java/com/example/demo/Controller/GradeController.java
@@ -3,7 +3,9 @@ package com.example.demo.Controller;
 import com.example.demo.Service.GradeService;
 import com.example.demo.model.Grade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,9 @@ public class GradeController {
         return gradeService.getAllGrades();
     }
 
+    @GetMapping("/{courseCode}")
+    public ResponseEntity<?> getGradesByCourseCode(@PathVariable String courseCode){
+        return gradeService.getGradesByCourseCode(courseCode);
+    }
 
 }
